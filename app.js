88b13befe20bccc51106003e389b065339ad893d
p-mazhnik <p-mazhnik@users.noreply.github.com>
2017-11-11 12:08:40 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
 var input_string = "Сделать задание #3 по web-программированию";
+
+var theInput = document.createElement('input');
+theInput.setAttribute('id', 'add_task_input');
+theInput.setAttribute('type', 'text');
+var add_task_button = document.createElement('button');
+add_task_button.setAttribute('id', 'add_task');
+add_task_button.addEventListener('click', add_task);
+add_task_button.innerHTML = "Добавить";
+document.getElementById("root").appendChild(theInput);
+document.getElementById("root").appendChild(add_task_button);
+/*$('#root').prepend($('#add_task')); // делаю эл-т первым
+$('#root').prepend($('#add_task_input'));*/
+
 add_my_tags(input_string);
 
 function add_my_tags(input_string) {
@@ -13,21 +26,9 @@ function add_my_tags(input_string) {
     theLi.appendChild(theSpan);
     theLi.appendChild(button);
     theUl.appendChild(theLi);
-    //document.getElementById("root").appendChild(theUl); //DOM API
-    $('#root').append(theUl); // jQuery
+    document.getElementById("root").appendChild(theUl); //DOM API
+    //$('#root').append(theUl); // jQuery
 }
-
-var theInput = document.createElement('input');
-theInput.setAttribute('id', 'add_task_input');
-theInput.setAttribute('type', 'text');
-var add_task_button = document.createElement('button');
-add_task_button.setAttribute('id', 'add_task');
-add_task_button.addEventListener('click', add_task);
-add_task_button.innerHTML = "Добавить";
-document.getElementById("root").appendChild(theInput);
-document.getElementById("root").appendChild(add_task_button);
-/*$('#root').prepend($('#add_task')); // делаю эл-т первым
-$('#root').prepend($('#add_task_input'));*/
 
 function add_task() {
     add_my_tags(theInput.value);
