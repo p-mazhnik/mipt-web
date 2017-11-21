1688c7ad3511303019d6313e3604f284bcf4e809
p-mazhnik <mazhnik.pa@phystech.edu>
2017-11-21 18:32:30 +0300

    update app.js (2)

--- task3/app.js
+++ task3/app.js
@@ -5,22 +5,20 @@ theInput.setAttribute('id', 'add_task_input');
 theInput.setAttribute('type', 'text');
 var add_task_button = document.createElement('button');
 add_task_button.setAttribute('id', 'add_task');
-add_task_button.onclick = add_task;
-add_task_button.innerHTML = "Добавить";
-theInput.onkeydown = function (e) {
-    if(e.keyCode == "13") {
-        document.getElementById('add_task').onclick();
-    }
+add_task_button.onclick = function () {
+    add_my_tags(theInput.value);
 };
+
+add_task_button.innerHTML = "Добавить";
+
 document.getElementById("root").appendChild(theInput);
 document.getElementById("root").appendChild(add_task_button);
 /*$('#root').prepend($('#add_task')); // делаю эл-т первым
 $('#root').prepend($('#add_task_input'));*/
-
+var theUl = document.createElement('ul');
 add_my_tags(input_string);
 
 function add_my_tags(input_string) {
-    var theUl = document.createElement('ul');
     var theLi = document.createElement('li');
     var theSpan = document.createElement('span');
     theSpan.innerHTML = input_string;
@@ -31,14 +29,22 @@ function add_my_tags(input_string) {
     theLi.appendChild(theSpan);
     theLi.appendChild(button);
     theUl.appendChild(theLi);
-    document.getElementById("root").appendChild(theUl); //DOM API
     //$('#root').append(theUl); // jQuery
 }
 
-function add_task() {
-    add_my_tags(theInput.value);
+function my_delete () {
+    //this.parentNode.parentNode.removeChild(this.parentNode);
+    this.remove();
 }
 
-function my_delete () {
-    this.parentNode.parentNode.removeChild(this.parentNode);
+document.getElementById("root").appendChild(theUl); //DOM API
+
+document.getElementById("body").addEventListener('keydown', enter_click);
+function enter_click (e) {
+    if(e.keyCode == "13") {
+        add_task_button.onclick();
+    }
+    if(e.keyCode == "46"){
+        theUl.remove();
+    }
 }
